ae612cd27dcce4fbb918c4fecdef8080e61a92a7
Dani <dani@pop-os.localdomain>
2023-10-21 12:07:08 +0200

    cleaned the main functions

--- extractor.py
+++ extractor.py
@@ -12,29 +12,43 @@ class News:
         # self.title = None
 
 
-def get_top_news():
-    # Realiza la solicitud GET
+def get_landing_page() -> str:
+    """
+        Gets the landing page in plain HTML as a string, for depth 0 news.
+        This is the starting point to get the first batch of links to news. 
+
+        Returns: string
+    """
     response = requests.get(BASE_URL)
 
-    # Verifica si la solicitud fue exitosa (código de respuesta 200)
     if response.status_code != 200:
         response = "error"
 
-    return response
+    return response.text
 
 
-def extract_related_links(html_text):
+def extract_related_links(html_text: str) -> list:
+    """
+        Extracts news links from a page, be it the landing page or any news page.
+        By news we understand exclusively this newspaper's articles.
+
+        Parameters: a string object 
+        Returns: list
+    """
     regex = r'<a[^>]*\s*href=["\'](https?://[^"\']+)["\']'
     matches = re.findall(regex, html_text)
 
-    related_links = [link for link in matches if 'www.20minutos.es' in link]
+    related_links = [link for link in matches if 'www.20minutos.es/noticia/' in link]
 
     return related_links
 
 
 if __name__ == '__main__':
-    top_news = get_top_news()
-    relevant_links = extract_related_links(top_news.text)
+    main_news = get_landing_page()
+    relevant_links = extract_related_links(main_news)
 
+    print("----------------------------------------------------------------------------------")
     print(relevant_links)    
+    print("----------------------------------------------------------------------------------")
     print(type(relevant_links))
+    print(len(relevant_links))
